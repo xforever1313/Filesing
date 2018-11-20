44aca650d1b36184b61510f79cb32fc59cf31fa5
Seth Hendrick <seth@shendrick.net>
2018-11-20 18:03:59 -0500

    Added empty projects

---
+++ Filesing.Api/PatternConfig.cs
@@ -0,0 +1,15 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+
+namespace Filesing.Api
+{
+    public class PatternConfig
+    {
+    }
+}
---
+++ Filesing.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+
+namespace Filesing.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
