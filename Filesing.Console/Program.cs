91c12b8250c9a6bf7274478cd973641dfdd8f6fb
Seth Hendrick <seth@shendrick.net>
2018-11-20 18:29:44 -0500

    Added formatting config

--- Filesing.Console/Program.cs
+++ Filesing.Console/Program.cs
@@ -11,9 +11,9 @@ namespace Filesing.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine( "Hello World!" );
         }
     }
 }
