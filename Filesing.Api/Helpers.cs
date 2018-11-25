5f573d28771dc6363348aaf1577562d20ce132f7
Seth Hendrick <seth@shendrick.net>
2018-11-25 18:24:49 -0500

    Added (untested) IgnoreConfig, which is used to determine if a file should be ignored or not.
    Added Sample XML file for the configuration of Filesing.

---
+++ Filesing.Api/Helpers.cs
@@ -0,0 +1,35 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Filesing.Api
+{
+    public static class Helpers
+    {
+        public static string NormalizePath( this string path )
+        {
+            // GetFullPath will replace something like C:\\\Hello World/there\..\there/file.exe with
+            // c:\Hello World\there\file.exe.
+            string fullPath = Path.GetFullPath( path );
+
+            // This will trim off something like:
+            // C:\users\hello\
+            // to
+            // C:\users\hello
+            return fullPath.TrimEnd( Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar );
+        }
+
+        public static bool ArePathsEqual( string path1, string path2 )
+        {
+            return NormalizePath( path1 ) == NormalizePath( path2 );
+        }
+    }
+}
---
+++ Filesing.Api/IgnoreConfig.cs
@@ -0,0 +1,166 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using SethCS.Exceptions;
+
+namespace Filesing.Api
+{
+    /// <summary>
+    /// This class specifies which files or directories should be ignored.
+    /// </summary>
+    public class IgnoreConfig
+    {
+        // ---------------- Fields ----------------
+
+        private readonly HashSet<string> ignoredFiles;
+        private readonly HashSet<string> ignoredDirectories;
+        private readonly List<Regex> ignoredFilesWithRegex;
+        private readonly List<Regex> ignoredDirsWithRegex;
+        private readonly List<Regex> ignoredFileExtensions;
+
+        // ---------------- Constructor ----------------
+
+        public IgnoreConfig()
+        {
+            this.ignoredFiles = new HashSet<string>();
+            this.ignoredDirectories = new HashSet<string>();
+
+            this.ignoredFilesWithRegex = new List<Regex>();
+            this.IgnoredFilesWithRegex = this.ignoredFilesWithRegex.AsReadOnly();
+
+            this.ignoredDirsWithRegex = new List<Regex>();
+            this.IgnoredDirectoriesWithRegex = this.ignoredDirsWithRegex.AsReadOnly();
+
+            this.ignoredFileExtensions = new List<Regex>();
+            this.IgnoredFileExtensions = this.ignoredFileExtensions.AsReadOnly();
+        }
+
+        // ---------------- Properties ----------------
+
+        /// <summary>
+        /// Specific files to ignore.  Case-sensitive.
+        /// </summary>
+        public IReadOnlyCollection<string> IgnoredFiles => this.ignoredFiles;
+
+        /// <summary>
+        /// Specific directories to ignore.  Case-sensitive.
+        /// </summary>
+        public IReadOnlyCollection<string> IgnoredDirectories => this.ignoredDirectories;
+
+        /// <summary>
+        /// If a file matches any of these regexes, it will be ignored.
+        /// </summary>
+        public IReadOnlyList<Regex> IgnoredFilesWithRegex { get; private set; }
+
+        /// <summary>
+        /// If a directory matches any of these regexes, it will be ignored.
+        /// </summary>
+        public IReadOnlyList<Regex> IgnoredDirectoriesWithRegex { get; private set; }
+
+        /// <summary>
+        /// If a file's extension matches any of these regexes, it will be ignored.
+        /// </summary>
+        public IReadOnlyList<Regex> IgnoredFileExtensions { get; private set; }
+
+        // ---------------- Functions ----------------
+
+        public void AddSpecificFileToIgnore( string file )
+        {
+            ArgumentChecker.StringIsNotNullOrEmpty( file, nameof( file ) );
+            this.ignoredFiles.Add( file.NormalizePath() );
+        }
+
+        public void AddSpecificDirToIgnore( string dir )
+        {
+            ArgumentChecker.StringIsNotNullOrEmpty( dir, nameof( dir ) );
+            this.ignoredDirectories.Add( dir.NormalizePath() );
+        }
+
+        public void AddFileRegexToIgnore( Regex fileRegex )
+        {
+            ArgumentChecker.IsNotNull( fileRegex, nameof( fileRegex ) );
+            this.ignoredFilesWithRegex.Add( fileRegex );
+        }
+
+        public void AddDirRegexToIgnore( Regex dirRegex )
+        {
+            ArgumentChecker.IsNotNull( dirRegex, nameof( dirRegex ) );
+            this.ignoredDirsWithRegex.Add( dirRegex );
+        }
+
+        public void AddIgnoredFileExtension( Regex extensionRegex )
+        {
+            ArgumentChecker.IsNotNull( extensionRegex, nameof( extensionRegex ) );
+            this.ignoredFileExtensions.Add( extensionRegex );
+        }
+
+
+        /// <summary>
+        /// Should we ignore the given path?
+        /// </summary>
+        /// <param name="path">The path to check</param>
+        /// <returns>True if the path should be ignored, else false.</returns>
+        public bool ShouldIgnore( string path )
+        {
+            path = path.NormalizePath();
+
+            // First are we a diretory or a file?
+            if( Directory.Exists( path ) )
+            {
+                if( this.IgnoredDirectories.Contains( path ) )
+                {
+                    return true;
+                }
+
+                foreach( Regex ignoredDir in this.IgnoredDirectoriesWithRegex )
+                {
+                    string dirName = Path.GetFileName( path );
+                    if( ignoredDir.IsMatch( dirName ) )
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if( File.Exists( path ) )
+            {
+                if( this.IgnoredFiles.Contains( path ) )
+                {
+                    return true;
+                }
+
+                foreach( Regex ignoredFileRegex in this.IgnoredFilesWithRegex )
+                {
+                    string fileName = Path.GetFileName( path );
+                    if( ignoredFileRegex.IsMatch( fileName ) )
+                    {
+                        return true;
+                    }
+                }
+
+                foreach( Regex extensionRegex in this.IgnoredFileExtensions )
+                {
+                    string extension = Path.GetExtension( path );
+                    if( extensionRegex.IsMatch( extension ) )
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                // Not a file or directory, ignore it.
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
