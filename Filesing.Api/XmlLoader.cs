261f0bfe5e0775ab364fe96d67e183af3a3083f6
Seth Hendrick <seth@shendrick.net>
2018-12-03 21:21:20 -0500

    Added stub for XmlLoader.

---
+++ Filesing.Api/XmlLoader.cs
@@ -0,0 +1,21 @@
+﻿//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filesing.Api
+{
+    public static class XmlLoader
+    {
+        public static FilesingConfig LoadConfigFromXml( string filePath )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
